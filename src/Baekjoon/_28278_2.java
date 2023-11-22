fa3b6c5ac8094a96dcc5fd9ccace8da5eabe6705
honeyWater <tkd2756@naver.com>
2023-11-22 20:41:38 +0900

    Baekjoon 28278

---
+++ src/Baekjoon/_28278_2.java
@@ -0,0 +1,32 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class _28278_2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine()); // 명령어의 수
+        Stack<Integer> stack = new Stack<>();
+        StringTokenizer st;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            switch (st.nextToken()) {
+                case "1" : stack.push(Integer.parseInt(st.nextToken())); break;
+                case "2" : sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n"); break;
+                case "3" : sb.append(stack.size()).append("\n"); break;
+                case "4" : sb.append(stack.isEmpty() ? 1 : 0).append("\n"); break;
+                case "5" : sb.append(stack.isEmpty() ? -1 : stack.peek()).append("\n"); break;
+                default: break;
+            }
+        }
+
+        br.close();
+        System.out.println(sb);
+    }
+}
---
+++ src/Baekjoon/_28278_3.java
@@ -0,0 +1,34 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class _28278_3 {
+
+    static StringBuilder sb = new StringBuilder();
+    static Stack<Integer> stack = new Stack<>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); // 명령어의 수
+        StringTokenizer st;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            switch (st.nextToken()) {
+                case "1" : stack.push(Integer.parseInt(st.nextToken())); break;
+                case "2" : sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n"); break;
+                case "3" : sb.append(stack.size()).append("\n"); break;
+                case "4" : sb.append(stack.isEmpty() ? 1 : 0).append("\n"); break;
+                case "5" : sb.append(stack.isEmpty() ? -1 : stack.peek()).append("\n"); break;
+                default: break;
+            }
+        }
+
+        br.close();
+        System.out.println(sb);
+    }
+}
