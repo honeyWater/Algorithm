f803318594eaaca684372d6bc247c33dac5d8991
honeyWater <tkd2756@naver.com>
2023-11-22 15:47:53 +0900

    Baekjoon 14425

---
+++ src/Baekjoon/_14425_1.java
@@ -0,0 +1,32 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _14425_1 {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        String[] S = new String[N];
+        int ans = 0;
+
+        for (int i = 0; i < N; i++) {
+            S[i] = br.readLine();
+        }
+
+        for (int i = 0; i < M; i++) {
+            String check = br.readLine();
+
+            for (int j = 0; j < N; j++) {
+                if (S[j].equals(check)) ans++;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
---
+++ src/Baekjoon/_14425_2.java
@@ -0,0 +1,33 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class _14425_2 {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int ans = 0;
+
+        Set<String> set = new HashSet<>();
+
+        // N만큼의 문자열 저장
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+
+        // 뒤의 M개의 문자열과 set을 비교
+        for (int i = 0; i < M; i++) {
+            ans = set.contains(br.readLine()) ? ans + 1 : ans;
+        }
+
+        System.out.println(ans);
+    }
+}
