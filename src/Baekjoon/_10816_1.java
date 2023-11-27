3349b78e2291648d5013444c184f8022924bf3bc
honeyWater <tkd2756@naver.com>
2023-11-27 21:45:10 +0900

    Baekjoon 10816

---
+++ src/Baekjoon/_10816_1.java
@@ -0,0 +1,49 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class _10816_1 {
+
+    static String num;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            num = st.nextToken();
+
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < M; i++) {
+
+            num = st.nextToken();
+
+            if (map.containsKey(num)) {
+                sb.append(map.get(num));
+            } else {
+                sb.append(0);
+            }
+
+            sb.append(' ');
+        }
+
+        System.out.println(sb);
+    }
+}
---
+++ src/Baekjoon/_10816_2.java
@@ -0,0 +1,29 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _10816_2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[20000001];
+
+        for (int i = 0; i < N; i++) {
+            arr[Integer.parseInt(st.nextToken()) + 10000000]++;
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < M; i++) {
+            sb.append(arr[Integer.parseInt(st.nextToken())+10000000]).append(' ');
+        }
+
+        System.out.println(sb);
+    }
+}
