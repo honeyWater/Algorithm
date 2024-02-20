eacaee04e1241ed851ac9bb3a597f2c15a56f4e3
honeyWater <tkd2756@naver.com>
2024-02-20 11:23:44 +0900

    Baekjoon - B2 2750번 수 정렬하기

---
+++ src/Baekjoon/P2750_수정렬하기_1.java
@@ -0,0 +1,27 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class P2750_수정렬하기_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
