095193ccede6d56b6103d2dc0b6d66f321991335
조은상 <99648793+honeyWater@users.noreply.github.com>
2024-03-19 16:07:23 +0900

    [level 0] Title: k의 개수, Time: 2.14 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static int idx;
+    
+    public static int haveI(int i, int k){
+        while(i > 0){
+            if(i % 10 == k) idx++;
+            i /= 10;
+        }
+        
+        return idx;
+    }
+    
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+        
+        for(int n = i;n<=j;n++){
+            idx = 0;
+            if(haveI(n,k) != 0){
+                answer += idx;
+            }
+        }
+        
+        return answer;
+    }
+}
