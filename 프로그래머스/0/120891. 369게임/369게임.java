b269deca7b19a83ef0cfe23ef494ff950a68eaeb
조은상 <99648793+honeyWater@users.noreply.github.com>
2024-03-19 16:14:53 +0900

    [level 0] Title: 369게임, Time: 8.63 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -3,10 +3,8 @@ class Solution {
         int answer = 0;
         String str = order + "";
         
-        for(int i=0; i<str.length(); i++){
-            if(order % 10 == 3
-              || order % 10 == 6
-              || order % 10 == 9)
+        for(int i=0; i < str.length(); i++){
+            if(order % 10 == 3 || order % 10 == 6 || order % 10 == 9)
                 answer++;
             order /= 10;
         }
