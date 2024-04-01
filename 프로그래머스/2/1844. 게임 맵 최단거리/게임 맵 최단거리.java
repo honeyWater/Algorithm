38d8ab1a08031106271f2a5362ec0b50b70ee7ef
조은상 <99648793+honeyWater@users.noreply.github.com>
2024-04-01 21:28:58 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.65 ms, Memory: 53.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+    
+    public int solution(int[][] maps) {
+        int answer = 0;
+        
+        int[][] visited = new int[maps.length][maps[0].length];
+        
+        bfs(maps, visited);
+        answer = visited[maps.length - 1][maps[0].length - 1];
+        
+        if(answer == 0) answer = -1;
+        
+        return answer;
+    }
+    
+    public void bfs(int[][] maps, int[][] visited){
+        int x = 0;
+        int y = 0;
+        visited[x][y] = 1;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+        
+        while(!q.isEmpty()){
+            int[] current = q.remove();
+            int cx = current[0];
+            int cy = current[1];
+            
+            for(int i=0; i<4; i++){
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+                
+                if(nx < 0 || nx > maps.length - 1 || ny < 0 || ny > maps[0].length - 1)
+                    continue;
+                
+                if(visited[nx][ny] == 0 && maps[nx][ny] == 1){
+                    visited[nx][ny] = visited[cx][cy] + 1;
+                    q.add(new int[]{nx, ny});
+                }
+            }
+        }
+    }
+}
