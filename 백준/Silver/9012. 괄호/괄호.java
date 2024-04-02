70e06e419091071ca1b88e6d8edae1116d99a761
조은상 <99648793+honeyWater@users.noreply.github.com>
2024-04-02 20:08:24 +0900

    [Silver IV] Title: 괄호, Time: 136 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,15 +1,18 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main {
     static Stack<Character> st;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
             st = new Stack<>();
-            String ps = sc.next();
+            String ps = br.readLine();
 
             for (int j = 0; j < ps.length(); j++) {
                 char c = ps.charAt(j);
