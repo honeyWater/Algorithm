cb3b2a1cfdf85a2cc48ea335a753831a7340cdc9
조은상 <99648793+honeyWater@users.noreply.github.com>
2024-04-12 19:38:50 +0900

    [level 0] Title: 안전지대, Time: 0.05 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -2,10 +2,10 @@ class Solution {
     static int[] dx = {-1, 0, 1, 1, 1, 0, -1, -1, 0};  // 왼쪽 대각선 부터 시계방향 + 지뢰 자리
     static int[] dy = {1, 1, 1, 0, -1, -1, -1, 0, 0};
 
-    public static int solution(int[][] board) {
+    public int solution(int[][] board) {
         int answer = 0;
         int n = board.length;
-        int[][] newBoard = new int[n][n];
+        boolean[][] newBoard = new boolean[n][n];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -14,7 +14,7 @@ class Solution {
                         int nx = i + dx[k];
                         int ny = j + dy[k];
                         if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-                            newBoard[nx][ny] = 1;
+                            newBoard[nx][ny] = true;
                         }
                     }
                 }
@@ -23,7 +23,7 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (newBoard[i][j] == 0) answer++;
+                if (!newBoard[i][j]) answer++;
             }
         }
 
