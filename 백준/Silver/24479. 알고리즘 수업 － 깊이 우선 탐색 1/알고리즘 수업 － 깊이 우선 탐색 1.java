4f77a439f163fb848676b1a2e934962ce751b5d3
조은상 <99648793+honeyWater@users.noreply.github.com>
2024-04-25 20:27:41 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1836 ms, Memory: 103036 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int cnt = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        int[] check = new int[n + 1];
+        List<Integer>[] graph = new List[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // 연결리스트 생성
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int[] edge = {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
+
+        // 각 정점에서 방문하는 순서는 작은 수 부터 되도록
+        // 간선이 없는 정점은 무시
+        for (int i = 0; i < n + 1; i++) {
+            if (graph[i] != null) {
+                Collections.sort(graph[i]);
+            }
+        }
+
+        dfs(check, graph, r);
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(check[i]);
+        }
+    }
+
+    static void dfs(int[] ch, List<Integer>[] graph, int v) {
+        ch[v] = ++cnt;
+
+        for (int nv : graph[v]) {
+            if (ch[nv] == 0) {
+                dfs(ch, graph, nv);
+            }
+        }
+    }
+}
