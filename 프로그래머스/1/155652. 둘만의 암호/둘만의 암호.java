af54c06ea271f4e48f1ffc48c2cf76666af1a7ba
조은상 <99648793+honeyWater@users.noreply.github.com>
2024-05-13 18:05:44 +0900

    [level 1] Title: 둘만의 암호, Time: 0.77 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String s, String skip, int index) {
+        StringBuilder answer = new StringBuilder();
+        
+        for(char ch : s.toCharArray()){
+            char temp = ch;
+            int idx = 0;
+            
+            // idx가 index가 될때까지 반복
+            while (idx < index){
+                temp = temp == 'z' ? 'a' : (char) (temp + 1);
+                
+                // temp가 skip문자가 아닐 때만 횟수 증가
+                if(!skip.contains(String.valueOf(temp))){
+                    idx += 1;
+                }
+            }
+            answer.append(temp);
+        }
+        
+        return answer.toString();
+    }
+}
