a8c3951b7da3dae65f58b78fa3e927a77d4582f7
조은상 <99648793+honeyWater@users.noreply.github.com>
2024-05-14 16:17:55 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 0.59 ms, Memory: 86.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        int[] alphaPos = new int[26];
+        
+        for(int i = 0; i < 26; i++) alphaPos[i] = -1;
+        
+        for(int i = 0; i < s.length(); i++){
+            int pos = s.charAt(i) - 97;
+            
+            if(alphaPos[pos] == -1){
+                answer[i] = -1;
+            } else {
+                answer[i] = i - alphaPos[pos];
+            }
+            
+            alphaPos[pos] = i;
+        }
+        
+        return answer;
+    }
+}
+// 알파벳 위치 체크 배열 선언 후 -1로 초기화
+// 값이 -1이면 answer[i] = -1, 아니면 (현재 위치 - 해당 위치)
+// 현재 위치로 업데이트, alphaPos[pos] = i 
