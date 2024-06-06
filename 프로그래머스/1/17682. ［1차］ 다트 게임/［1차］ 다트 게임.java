eebefa4512fc903e6676a262b2f9daf5f1e49284
조은상 <99648793+honeyWater@users.noreply.github.com>
2024-06-06 20:16:07 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.06 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int solution(String dartResult) {
+        int[] num = new int[3];
+        int len = dartResult.length();
+
+        int n = 0;
+        for (int i = 0; i < len; i++) {
+            char temp = dartResult.charAt(i);
+            switch (temp) {
+                case 'S':
+                    break;
+                case 'D':
+                    num[n - 1] = (int) Math.pow(num[n - 1], 2);
+                    break;
+                case 'T':
+                    num[n - 1] = (int) Math.pow(num[n - 1], 3);
+                    break;
+                case '#':
+                    num[n - 1] = -1 * num[n - 1];
+                    break;
+                case '*':
+                    if (n >= 2) {
+                        num[n - 2] = 2 * num[n - 2];
+                    }
+                    num[n - 1] = 2 * num[n - 1];
+                    break;
+                default:
+                    if (isDigit(dartResult.charAt(i + 1))) {
+                        num[n++] = 10;
+                        i++;
+                    } else {
+                        num[n++] = temp - '0';
+                    }
+                    break;
+            }
+        }
+
+        return num[0] + num[1] + num[2];
+    }
+    
+    private static boolean isDigit(char c) {
+        c -= '0';
+        if (c >= 0 && c <= 9) {
+            return true;
+        }
+        return false;
+    }
+}
