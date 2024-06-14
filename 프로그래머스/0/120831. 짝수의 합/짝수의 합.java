132e8d7eed80be4c3f0d80fa6ce0be0ef06486fc
조은상 <99648793+honeyWater@users.noreply.github.com>
2024-06-14 23:06:11 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        if(n % 2 == 1) n -= 1;
+        
+        while(n > 0){
+            answer += n;
+            n -= 2;
+        }
+        
+        return answer;
+    }
+}
