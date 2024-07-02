38808c1d033b256220b80ab30e89632bb21d5968
조은상 <99648793+honeyWater@users.noreply.github.com>
2024-07-02 22:58:55 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -8,7 +8,6 @@ class Solution {
             } else {
                 num = 3 * num + 1;
             }
-            System.out.println(num);
             if (num == 1) return i;
         }
         
