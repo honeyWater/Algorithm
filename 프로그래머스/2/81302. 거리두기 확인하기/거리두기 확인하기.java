e6b60816b6d62e992c6c689918fec93a6cf0c29c
EunSang Cho <99648793+honeyWater@users.noreply.github.com>
2025-01-13 21:09:41 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.11 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,5 +1,15 @@
 import java.util.*;
 
+/*
+0. places 별로 메서드를 돌려서
+1. P의 좌표를 갖고 있는다.
+2. 각 P의 좌표를 비교해서 맨해튼 거리가 2 초과면 continue
+2. 각 P의 좌표를 비교해서 맨해튼 거리가 1이면 return 0
+3. 각 P의 좌표를 비교해서 맨해튼 거리가 2이면
+- 같은 행이나 열이면, 그 사이에 파티션이 있는가?
+- 대각이라면, 다른 대각에 파티션이 있는가?
+*/
+
 class Solution {
     private static int[] answer;
     
@@ -37,27 +47,33 @@ class Solution {
                 } else if (manhattan == 1) {
                     return 0;
                 } else if (manhattan == 2) {
-                    // 같은 행이나 열이면 그 사이에 파티션이 없으면 거리두기 X
-                    if (listA[0] == listB[0]) {
-                        if (place[listA[0]].charAt(Math.min(listA[1], listB[1]) + 1) == 'O') {
-                            return 0;
-                        }
-                    } else if (listA[1] == listB[1]) {
-                        if (place[Math.min(listA[0], listB[0]) + 1].charAt(listA[1]) == 'O') {
-                            return 0;
-                        }
-                    }
-                    // 대각인데, 다른 대각에 파티션이 없으면 거리두기 X
-                    else {
-                        if (place[listA[0]].charAt(listB[1]) == 'O' ||
-                            place[listB[0]].charAt(listA[1]) == 'O') {
-                            return 0;
-                        }
-                    }
+                    if (!checkPartician(place, listA, listB)) return 0;
                 }
             }
         }
 
         return 1;
+    }
+    
+    private static boolean checkPartician(String[] place, int[] listA, int[] listB) {
+        // 같은 행이나 열이면 그 사이에 파티션이 없으면 거리두기 X
+        if (listA[0] == listB[0]) {
+            if (place[listA[0]].charAt(Math.min(listA[1], listB[1]) + 1) == 'O') {
+                return false;
+            }
+        } else if (listA[1] == listB[1]) {
+            if (place[Math.min(listA[0], listB[0]) + 1].charAt(listA[1]) == 'O') {
+                return false;
+            }
+        }
+        // 대각인데, 다른 대각에 파티션이 없으면 거리두기 X
+        else {
+            if (place[listA[0]].charAt(listB[1]) == 'O' ||
+                place[listB[0]].charAt(listA[1]) == 'O') {
+                    return false;
+            }
+        }
+        
+        return true;
     }
 }
