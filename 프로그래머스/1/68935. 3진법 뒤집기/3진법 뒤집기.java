fd21b47829082af24d7c3a641735b453d70a9320
EunSang Cho <99648793+honeyWater@users.noreply.github.com>
2025-01-20 17:28:50 +0900

    [level 1] Title: 3진법 뒤집기, Time: 12.05 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,25 +1,13 @@
 class Solution {
     public int solution(int n) {
-        StringBuffer sb = new StringBuffer();
-        int answer = 0;
+        String s = "";
         
-        while (true) {
-            if (n < 3) {
-                sb.append(n);
-                break;
-            }
-            
-            sb.append(n % 3);
+        while (n > 0) {
+            s = (n % 3) + s;
             n /= 3;
         }
+        s = new StringBuilder(s).reverse().toString();
         
-        String s = sb.toString();
-        int len = s.length();
-        
-        for (int i = 0; i < len; i++) {
-            answer += Math.pow(3, i) * (s.charAt(len - 1 - i) - '0');
-        }
-        
-        return answer;
+        return Integer.parseInt(s, 3);
     }
 }
