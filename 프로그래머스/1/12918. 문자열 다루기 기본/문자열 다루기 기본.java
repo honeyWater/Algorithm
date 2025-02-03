498a4de62cb09e28d228e43d335ae37ed3544b05
EunSang Cho <99648793+honeyWater@users.noreply.github.com>
2025-02-03 21:11:02 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.07 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,5 +1,15 @@
 class Solution {
     public boolean solution(String s) {
-        return s.matches("^\\d{4}$|^\\d{6}$");
+        if (s.length() != 4 && s.length() != 6) return false;
+        
+        char[] chArr = s.toCharArray();
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (!Character.isDigit(chArr[i])) {
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
