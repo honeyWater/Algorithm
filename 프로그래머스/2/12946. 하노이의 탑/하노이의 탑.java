8e5687e4442bcede875be0b51c9b49d471eff285
EunSang Cho <99648793+honeyWater@users.noreply.github.com>
2025-02-17 23:23:37 +0900

    [level 2] Title: 하노이의 탑, Time: 4.85 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    static List<int[]> arr = new ArrayList<>();
+    
+    public int[][] solution(int n) {
+        hanoi(n, 1, 2, 3);
+        
+        return arr.stream().toArray(int[][]::new);
+    }
+    
+    static void hanoi(int cnt, int start, int mid, int end) {
+        if (cnt == 0) return;
+        
+        hanoi(cnt - 1, start, end, mid);
+        arr.add(new int[]{start, end});
+        hanoi(cnt - 1, mid, start, end);
+    }
+}
