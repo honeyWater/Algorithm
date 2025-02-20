60ede39103e5d51262b1d516f0973ee26e6cde21
EunSang Cho <99648793+honeyWater@users.noreply.github.com>
2025-02-20 20:51:53 +0900

    [level 2] Title: 하노이의 탑, Time: 5.19 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -12,8 +12,14 @@ class Solution {
     static void hanoi(int cnt, int start, int mid, int end) {
         if (cnt == 0) return;
         
+        // A -> C로 옮긴다고 가정할 때,
+	    // STEP 1 : N-1개를 A에서 B로 이동 (= start 지점의 N-1개의 원판을 mid 지점으로 옮긴다.)
         hanoi(cnt - 1, start, end, mid);
+        
+        // STEP 2 : 1개를 A에서 C로 이동 (= start 지점의 N번째 원판을 end지점으로 옮긴다.)
         arr.add(new int[]{start, end});
+        
+        // STEP 3 : N-1개를 B에서 C로 이동 (= mid 지점의 N-1개의 원판을 end 지점으로 옮긴다.)
         hanoi(cnt - 1, mid, start, end);
     }
 }
