866bbc432dbe5ded9ac43aeabca0fd7df1f125fe
EunSang Cho <99648793+honeyWater@users.noreply.github.com>
2025-03-03 17:52:31 +0900

    [level 1] Title: 모의고사, Time: 0.83 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -5,29 +5,31 @@ class Solution {
         int[] num1 = {1, 2, 3, 4, 5};
         int[] num2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] num3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int[] cnt = new int[3];
+        
+        int score1 = 0, score2 = 0, score3 = 0;
         
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == num1[i % 5]) {
-                cnt[0]++;
-            }
-            if (answers[i] == num2[i % 8]) {
-                cnt[1]++;
-            }
-            if (answers[i] == num3[i % 10]) {
-                cnt[2]++;
-            }
+            int answer = answers[i];
+            
+            if (answer == num1[i % 5]) score1++;
+            if (answer == num2[i % 8]) score2++;
+            if (answer == num3[i % 10]) score3++;
         }
         
-        int max = Math.max(cnt[0], Math.max(cnt[1], cnt[2]));
+        int maxScore = Math.max(score1, Math.max(score2, score3));
         
-        List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            if (cnt[i] == max) {
-                answer.add(i + 1);
-            }
-        }
+        int count = 0;
+        if (score1 == maxScore) count++;
+        if (score2 == maxScore) count++;
+        if (score3 == maxScore) count++;
         
-        return answer.stream().mapToInt(i->i.intValue()).toArray();
+        int[] result = new int[count];
+        int idx = 0;
+        
+        if (score1 == maxScore) result[idx++] = 1;
+        if (score2 == maxScore) result[idx++] = 2;
+        if (score3 == maxScore) result[idx++] = 3;
+        
+        return result;
     }
 }
