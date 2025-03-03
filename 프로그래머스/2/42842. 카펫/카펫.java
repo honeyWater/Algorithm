0e34005e61e742e44472222c6599b326eed0248d
EunSang Cho <99648793+honeyWater@users.noreply.github.com>
2025-03-03 17:48:58 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,29 @@
+// 1. brown과 yellow를 더한 값 = 넓이
+// 2. 넓이의 가로 길이는 세로 길이와 같거나, 세로 길이보다 길다.
+// 3. 중앙 노란색의 넓이는 가로-2 * 세로-2
+// 4. yellow, brown이 최솟값일 때, 가로 세로는 최소 3
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int area = brown + yellow;
+
+        for(int y = 3; y < area; y++){  // x, y 둘 다 최소 3
+            int x = area / y;
+
+            // i로 나누어떨어지고 j가 3보다는 크다면
+            if(area % y == 0 && x >= y){
+                int center = (x - 2) * (y - 2);
+
+                // 노란색이 가운데 들어갈 수 있는지
+                if(center == yellow){
+                    answer[0] = x;
+                    answer[1] = y;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
